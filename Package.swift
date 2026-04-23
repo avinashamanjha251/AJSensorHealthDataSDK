b79db2ef44c0c681c14061073a101365a4d0c7c7
Avinash Aman <avinashamanjha@gmail.com>
2026-04-23 13:31:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AJSensorHealthData",
-            url: "https://github.com/avinashamanjha251/AJSensorHealthDataSDK/releases/download/1.0.6/AJSensorHealthData.xcframework.zip",
+            url: "https://github.com/avinashamanjha251/AJSensorHealthDataSDK/releases/download/1.0.7/AJSensorHealthData.xcframework.zip",
             checksum: "2d5dcca268fa991c44da8359ed7c9356555cd1b8d7665914de278ec7c47f2d10"
         )
     ]
