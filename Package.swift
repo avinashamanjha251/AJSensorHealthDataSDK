917f5c10e909c7001b9679de50be6183d43c76d3
Avinash Aman <avinashamanjha@gmail.com>
2026-04-23 12:53:44 +0530

    release: v1.0.4 — update binary checksum

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AJSensorHealthData",
-            url: "https://github.com/avinashamanjha251/AJSensorHealthDataSDK/releases/download/1.0.3/AJSensorHealthData.xcframework.zip",
-            checksum: "347d0013505085601e5b18bb957a92fbc748362ef975794aca9293738079a1e9"
+            url: "https://github.com/avinashamanjha251/AJSensorHealthDataSDK/releases/download/1.0.4/AJSensorHealthData.xcframework.zip",
+            checksum: "088a18be66467545f0d226e874f4b656d0973543a2caf00cff27a7e5810daee9"
         )
     ]
 )
